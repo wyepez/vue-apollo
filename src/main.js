887629a3d613acea9fd97d3f068eaa4803d00e2b
Washington Xavier Yépez Yépez <wyepez@outlook.com>
2020-07-21 17:47:13 -0500

    initial commit

---
+++ src/main.js
@@ -0,0 +1,52 @@
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import AuthPlugin from "./plugins/auth";
+
+import VueApollo from "vue-apollo";
+
+import ApolloClient from 'apollo-client'
+import { WebSocketLink } from "apollo-link-ws";
+import { InMemoryCache } from "apollo-cache-inmemory";
+
+Vue.use(AuthPlugin);
+Vue.use(VueApollo)
+
+Vue.config.productionTip = false;
+const getHeaders = () => {
+  const headers = {}
+  const token = window.localStorage.getItem('apollo-token')
+  if (token) {
+    headers.authorization = `Bearer ${token}`
+  }
+  return headers
+}
+
+// Create a WebSocket link:
+const link = new WebSocketLink({
+  uri: 'wss://hasura.io/learn/graphql',
+  options: {
+    reconnect: true,
+    timeout: 30000,
+    connectionParams: () => {
+      return { headers: getHeaders() }
+    }
+  }
+})
+
+const client = new ApolloClient({
+  link: link,
+  cache: new InMemoryCache({
+    addTypename: true
+  })
+})
+
+const apolloProvider = new VueApollo({
+  defaultClient: client
+})
+
+new Vue({
+  router,
+  apolloProvider,
+  render: h => h(App)
+}).$mount("#app");
